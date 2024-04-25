2fa5a77871f511e572263f8613a2fb7600131b95
Yaroslav Nyanyushkin <108981413+MrMitrum@users.noreply.github.com>
2024-04-25 13:44:09 +0400

    Project

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿namespace ConsoleAppGitEdukov
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double d;
+            Console.WriteLine("Введите A");
+            double z = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите B");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Выполнить +,-,*,:");
+            char c = Convert.ToChar(Console.ReadLine());
+            if(c == '+')
+            {
+                d = z + x;
+                Console.WriteLine(d);
+
+            }
+            else if(c == '-')
+            {
+                d = z - x;
+                Console.WriteLine(d);
+            }
+            else if(c == '*')
+            {
+                d = z * x;
+                Console.WriteLine(d);
+            }
+            else if(c == ':')
+            {
+                d = z / x;
+                Console.WriteLine(d);
+            }
+            
+           
+           
+
+
+        }
+    }
+}
